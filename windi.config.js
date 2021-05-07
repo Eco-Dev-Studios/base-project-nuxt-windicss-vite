a53bbd7895873277fe157e481fb6a12c3adb86de
cesswhite <acessloop@gmail.com>
2021-05-07 17:44:07 -0500

    :tada: First commit of v2

---
+++ windi.config.js
@@ -0,0 +1,16 @@
+const colors = require("windicss/colors");
+const typography = require("windicss/plugin/typography");
+
+module.exports = {
+  darkMode: "class",
+  plugins: [typography],
+  shortcuts: {},
+  theme: {
+    extend: {
+      colors: {
+        teal: colors.teal,
+        gray: colors.gray
+      }
+    }
+  }
+};
